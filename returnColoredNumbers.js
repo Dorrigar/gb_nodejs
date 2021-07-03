f2c4b7eed3c48c64f77d56c3c083138cc397d36b
p-vasich <Dorrigar@gmail.com>
2021-07-03 19:11:43 +0300

    add checking empty array

--- returnColoredNumbers.js
+++ returnColoredNumbers.js
@@ -2,14 +2,15 @@ const colors = require('colors/safe')
 const firstNumber = process.argv[2]
 const secondNumber = process.argv[3]
 
-if (isNaN(firstNumber) || isNaN(secondNumber) || firstNumber === secondNumber) {
-    console.log(colors.red('Введите два числа, они должы быть разными'))
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    console.log(colors.red('Введите два числа, хотя бы одно введенное значение не является числом'))
 } else {
-    const simpleNumbers = getPrimeNumbers (firstNumber, secondNumber)
-    logWithColors(simpleNumbers)
+    const simpleNumbers = getPrimeNumbers(firstNumber, secondNumber)
+    if (simpleNumbers.length === 0) console.log(colors.red('В выбранный диапазон не попало ни одного простого числа'))
+    else logWithColors(simpleNumbers)
 }
 
-function getPrimeNumbers (firstNumber, secondNumber) {
+function getPrimeNumbers(firstNumber, secondNumber) {
     let array = []
     const min = firstNumber < secondNumber ? firstNumber : secondNumber
     const max = firstNumber > secondNumber ? firstNumber : secondNumber
@@ -21,8 +22,8 @@ function getPrimeNumbers (firstNumber, secondNumber) {
 }
 
 function isPrime(num) {
-    for(let i = 2; i < num; i++)
-        if(num % i === 0) return false;
+    for (let i = 2; i < num; i++)
+        if (num % i === 0) return false;
     return num > 1;
 }
 
@@ -30,10 +31,15 @@ function logWithColors(numbers) {
     let i = 1
     numbers.forEach((number) => {
         switch (i) {
-            case 1: console.log(colors.green(number)); break
-            case 2: console.log(colors.yellow(number)); break
-            case 3: console.log(colors.red(number))
-                    i =0; break
+            case 1:
+                console.log(colors.green(number));
+                break
+            case 2:
+                console.log(colors.yellow(number));
+                break
+            default:
+                console.log(colors.red(number))
+                i = 0
         }
         i++
     })
