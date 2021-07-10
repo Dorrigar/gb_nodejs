1c5ed01bd0f453a99f03f18373ad6fb6af6c4c66
p-vasich <Dorrigar@gmail.com>
2021-07-10 20:02:09 +0300

    add events

--- timer.js
+++ timer.js
@@ -1,73 +1,63 @@
-const colors = require('colors/safe')
-const second = 1000 //для ускорения измените на 1
-const inputParameters = process.argv
+const EventEmitter = require('events'),
+    colors = require('colors/safe'),
+    second = 1000, //для ускорения измените на 1
+    inputParameters = process.argv?.slice(2)
 
-//исключаю первые параметры чтобы на них не срабатывала ошибка ввода
-inputParameters.shift()
-inputParameters.shift()
+let emitterObject = new EventEmitter(),
+    proceed = []
 
 inputParameters.forEach((parameter, index) => {
-    setTimeout(() => {
-        let dateTime = parameter.split('-')
-
-        if (checkFormat(dateTime)) {
-            timer(dateTime, 'Timer N' + index)
-
-        } else console.log(colors.red('Вводите параметры формы «секунды-часы-дни-месяцы-годы»'))
-    }, 0)
-})
-
-
-function timer(dateTime, name) {
+    let dateTime = parameter.split('-')
+    const name = 'Timer N' + index
+    proceed[index] = true
     console.log('Запуск таймера', name, ':', colors.yellow(timerMessage(dateTime)))
-    clock()
 
-    function clock() {
-        setTimeout(() => {
+    if (checkFormat(dateTime)) {
+        emitterObject.on('clock', () => {
             dateTime = minusSecond(dateTime)
 
-            if (timerMessage(dateTime)) {
-                console.log(colors.red(name), timerMessage(dateTime))
-                clock(dateTime, name)
-            } else console.log(colors.green(name + ' finished'))
-        }, second)
+            if (proceed[index] === true ) {
+                if (timerMessage(dateTime)) {
+                    console.log(colors.red(name), timerMessage(dateTime))
+                } else {
+                    proceed[index] = false
+                    console.log(colors.green(name + ' finished'))
+                }
+            }
+        })
+    } else {
+        proceed[index] = false
+        console.log(colors.red('Вводите параметры формы «секунды-часы-дни-месяцы-годы»'))
     }
-}
+})
+
+let interval = setInterval(() => {
+    emitterObject.emit('clock')
+    if (proceed.every((element) => { return element !== true })) clearInterval(interval)
+}, second)
 
 function minusSecond(dateTime) {
     if (dateTime) {
-        if (dateTime[0] <= 0) {
-            if (dateTime[1] && dateTime[1] <= 0) {
-                if (dateTime[2] && dateTime[2] <= 0) {
-                    if (dateTime[3] && dateTime[3] <= 0) {
-                        if (dateTime[4] && dateTime[4] <= 0) {
-                            if (dateTime[5] && dateTime[5] <= 0) {
-                                return null
-                            } else {
-                                dateTime[5]--
-                                dateTime[4] += 12
-                            }
-                        } else {
-                            dateTime[4]--
-                            dateTime[3] += 30
-                        }
-                    } else {
-                        dateTime[3]--
-                        dateTime[2] += 24
-                    }
-                } else {
-                    dateTime[2]--
-                    dateTime[1] += 60
-                }
-            } else {
-                dateTime[1]--
-                dateTime[0] += 60
-            }
-        } else dateTime[0]--
+        if (dateTime[0] && dateTime[0] > 0) dateTime[0]--
+        else if (dateTime[1] && dateTime[1] > 0) {
+            dateTime[1]--
+            dateTime[0] = 60
+        } else if (dateTime[2] > 0) {
+            dateTime[1]--
+            dateTime[0] += 60
+        } else if (dateTime[3] > 0) {
+            dateTime[3]--
+            dateTime[2] += 24
+        } else if (dateTime[4] > 0) {
+            dateTime[4]--
+            dateTime[3] += 30
+        } else if (dateTime[5] > 0) {
+            dateTime[5]--
+            dateTime[4] += 12
+        } else dateTime = null
     }
     return dateTime
 }
-
 
 function checkFormat(array) {
     return Array.isArray(array) && array.every((element) => {
