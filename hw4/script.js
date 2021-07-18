010052a94bdb0ee81f3daa429707dbb4a2daff62
p-vasich <Dorrigar@gmail.com>
2021-07-18 17:00:32 +0300

    HW4 completed

--- hw4/script.js
+++ hw4/script.js
@@ -1,27 +1,63 @@
-//Дз заглушка
+#!/usr/bin/env node
 
-const fs = require('fs'),
-    path = require('path')
-
-const    yargs = require('yargs')
-
-fs.readFile("./access.log","utf8", (err, data) => {
-    console.log(data);
-})
+const fs = require("fs"),
+    path = require("path"),
+    yargs = require('yargs'),
+    readline = require("readline"),
+    inquirer = require("inquirer")
 
 const options = yargs
-    .usage("Usage: -p <path>")
-    .option("p", { alias: "path", describe: "Path to file", type: "string", demandOption: true })
-    .argv;
+        .usage('Usage: -p <path>')
+        .option("p", {alias: "path", describe: "Path to file", type: "string", demandOption: false})
+        .argv,
 
-const filePath = path.join(__dirname, options.path);
+    folderPath = path.join(process.cwd(), options.path || "")
 
-console.log('log')
+fileRequest(folderPath)
 
+function fileRequest(folderPath) {
+    let list = fs.readdirSync(folderPath)
+    list.unshift("..")
 
-//Для этого превратите её в консольное приложение, по аналогии с разобранным примером и добавьте следующие функции:
-// * Возможность передавать путь к директории в программу.
-// Это актуально, когда вы не хотите покидать текущую директорию, но вам необходимо просмотреть файл,
-// находящийся в другом месте;
-// * В содержимом директории переходить во вложенные каталоги;
-// * При чтении файлов искать в них заданную строку или паттерн.
+    inquirer
+        .prompt([
+            {
+                name: "fileName",
+                type: "list",
+                message: "Choose file:",
+                choices: list,
+            },
+        ])
+
+        .then((answer) => {
+            const filePath = path.join(folderPath, answer.fileName)
+            isFile(filePath) ? readFile(filePath) : fileRequest(filePath)
+        })
+}
+
+function isFile(filePath) {
+    return fs.lstatSync(filePath).isFile()
+}
+
+function readFile(filePath) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    rl.question("Укажите что вы хотите найти в документе: ", function (query) {
+        logFilePartWithQuery(filePath, query)
+        rl.close()
+    })
+}
+
+function logFilePartWithQuery(filePath, query) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+
+        const lines = data.split('\n')
+        const result = lines.filter((value) => {
+            return value.includes(query)
+        })
+        console.log(result.join('\n'))
+    })
+}
